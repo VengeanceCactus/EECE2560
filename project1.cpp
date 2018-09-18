cd2fad7a541926bc7b65d1ab6ba48632be241898
VengeanceCactus <31999886+VengeanceCactus@users.noreply.github.com>
2018-09-17 20:20:44 -0400

    Rough framework for Project 1a

---
+++ project1.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+
+class code{
+private:
+    int n, m;
+public:
+    code(n,m)
+
+int checkCorrect{
+    int correct[n];
+    for(int i = 0; i<(n-1); i++)
+        result = std::equal(Secret.begin() + i, Secret.begin() + i, Guess.begin() + i);
+        if(result)
+            correct[i] = 1;
+        else
+            correct[i] = 0;
+}
+
+int checkIncorrect{
+    int incorrect[n];
+
+}
+}
+
+int main(){
+
+int n = 5;
+int m = 7;
+std::vector<int> Secret;
+for(int i = 0; i<n; i++)
+{
+    Secret.push_back(rand() % m);
+}
+std::vector<int> Guess;
+/*for (std::vector<int>::iterator it = Secret.begin(); it != Secret.end(); ++it)
+  std::cout << ' ' << *it;
+std::cout << '\n';*/
+
+
+//sample guess codes
+int Guess1 = {5, 0, 3, 2, 6}
+int Guess2 = {2, 1, 2, 2, 2}
+int Guess3 = {1, 3, 3, 4, 5}
+
+}
